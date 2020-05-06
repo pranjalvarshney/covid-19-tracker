841d44dd90406577aeff1947f3591c02fb08534d
Pranjal Varshney <Pranjal Varshney>
2020-05-06 23:20:03 +0530

    Updates

---
+++ precache-manifest.440b768fada6b1057d59f4517f3a6fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c2d9b60869ee27ed12d1a7753420dd",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "115b14e7e9fc5bf6809f",
+    "url": "/covid-19-tracker/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7ae1989ce07f08df93bc",
+    "url": "/covid-19-tracker/static/css/main.6b0d0290.chunk.css"
+  },
+  {
+    "revision": "115b14e7e9fc5bf6809f",
+    "url": "/covid-19-tracker/static/js/2.d251cf98.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/covid-19-tracker/static/js/2.d251cf98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae1989ce07f08df93bc",
+    "url": "/covid-19-tracker/static/js/main.66cc961e.chunk.js"
+  },
+  {
+    "revision": "c06afc726eae28e4038f",
+    "url": "/covid-19-tracker/static/js/runtime-main.9d938666.js"
+  }
+]);
